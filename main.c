b83da4adce491848f8a81df11b9d4dd0b0ae48a5
Simon Julian Lauw <simonjulian21@gmail.com>
2023-04-03 00:26:12 +0800

    Finish task 2.1 inspection

--- main.c
+++ main.c
@@ -9,32 +9,88 @@
 
 #include "fat.h"
 
+#define BPB_SIZE sizeof(struct BPB)
+
+void inspect_fat(const char *diskimg_path);
+
+uint32_t get_fat_sector_size(const struct BPB *hdr);
+
+uint32_t get_root_dir_sectors(const struct BPB *hdr);
+
+uint32_t get_data_sectors(const struct BPB *hdr, uint32_t root_dir_sectors, uint32_t fat_size);
+
+uint32_t get_total_sectors(const struct BPB *hdr);
+
 /*
  * Check if the given disk image is a FAT32 disk image.
  * Return true if it is, false otherwise.
- *
- * WARNING: THIS FUNCTION IS FOR DEMONSTRATION PURPOSES ONLY!
- * This function uses an external command called "file" to check the file
- * system, but your program shouldn't rely on outside programs. You need to
- * create your own function to check the file system type and delete this
- * function before submitting. If you use this function in your submission,
- * you'll get a penalty.
- *
- * RTFM: Section 3.5
  */
-bool check_fat32(const char *disk) {
-#warning "You must remove this function before submitting."
-    char buf[PATH_MAX];
-    FILE *proc;
-
-    snprintf(buf, PATH_MAX, "file %s", disk);
-    proc = popen(buf, "r");
-    if (proc == NULL) {
-        perror("popen");
+int32_t get_fat_version(const char *disk) {
+    int fd;
+    fd = open(disk, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
         exit(1);
     }
-    fread(buf, 1, PATH_MAX, proc);
-    return strstr(buf, "FAT (32 bit)") != NULL;
+
+    uint8_t *image = mmap(NULL, BPB_SIZE, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (image == (void *) -1) {
+        perror("mmap");
+        exit(1);
+    }
+    close(fd);
+
+    const struct BPB *hdr = (const struct BPB *) image;
+
+    /*
+     * Manual 3.5: Determination of FAT type when mounting the volume
+     * The FAT type is determined solely by the count of clusters on the volume (CountOfClusters).
+     */
+    uint32_t root_dir_sectors = get_root_dir_sectors(hdr);
+    uint32_t fat_size, data_sectors, count_of_clusters;
+    fat_size = get_fat_sector_size(hdr);
+    data_sectors = get_data_sectors(hdr, root_dir_sectors, fat_size);
+    count_of_clusters = data_sectors / hdr->BPB_SecPerClus;
+
+    munmap(image, BPB_SIZE);
+    if (count_of_clusters < 4085) {
+        return 12;
+    } else if (count_of_clusters < 65525) {
+        return 16;
+    } else {
+        return 32;
+    }
+}
+
+uint32_t get_data_sectors(const struct BPB *hdr, uint32_t root_dir_sectors, uint32_t fat_size) {
+    uint32_t data_sectors, total_sectors;
+    total_sectors = get_total_sectors(hdr);
+    data_sectors = total_sectors - (hdr->BPB_RsvdSecCnt + hdr->BPB_NumFATs * fat_size) + root_dir_sectors;
+    return data_sectors;
+}
+
+uint32_t get_total_sectors(const struct BPB *hdr) {
+    uint32_t total_sectors;
+    if (hdr->BPB_TotSec16 != 0) {
+        total_sectors = hdr->BPB_TotSec16;
+    } else {
+        total_sectors = hdr->BPB_TotSec32;
+    }
+    return total_sectors;
+}
+
+uint32_t get_root_dir_sectors(const struct BPB *hdr) {
+    return ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec - 1)) / hdr->BPB_BytsPerSec;
+}
+
+uint32_t get_fat_sector_size(const struct BPB *hdr) {
+    uint32_t fat_size;
+    if (hdr->BPB_FATSz16 != 0) {
+        fat_size = hdr->BPB_FATSz16;
+    } else {
+        fat_size = hdr->fat32.BPB_FATSz32;
+    }
+    return fat_size;
 }
 
 /*
@@ -61,28 +117,21 @@ void hexdump(const void *data, size_t size) {
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s disk.img ck\n", argv[0]);
+        fprintf(stderr, "Usage: %s disk.img <ck|ls> \n", argv[0]);
         exit(1);
     }
-    const char *diskimg = argv[1];
+    const char *diskimg_path = argv[1];
+    const char *op = argv[2];
 
-    /*
-     * This demo program only works on FAT32 disk images.
-     */
-    if (!check_fat32(argv[1])) {
-        fprintf(stderr, "%s is not a FAT32 disk image\n", diskimg);
-        exit(1);
+    if (strcmp(op, "ck") == 0) {
+        inspect_fat(diskimg_path);
+    } else {
+
     }
+}
 
-    /*
-     * Open the disk image and map it to memory.
-     *
-     * This demonstration program opens the image in read-only mode, which means
-     * you won't be able to modify the disk image. However, if you need to make
-     * changes to the image in later tasks, you should open and map it in
-     * read-write mode.
-     */
-    int fd = open(diskimg, O_RDONLY);
+void inspect_fat(const char *diskimg_path) {
+    int fd = open(diskimg_path, O_RDONLY);
     if (fd < 0) {
         perror("open");
         exit(1);
@@ -94,7 +143,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     uint8_t *image = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (image == (void *)-1) {
+    if (image == (void *) -1) {
         perror("mmap");
         exit(1);
     }
@@ -103,19 +152,17 @@ int main(int argc, char *argv[]) {
     /*
      * Print some information about the disk image.
      */
-    const struct BPB *hdr = (const struct BPB *)image;
-    printf("FAT%d filesystem\n", 32);
+    const struct BPB *hdr = (const struct BPB *) image;
+    uint32_t fat_size = get_fat_sector_size(hdr);
+    uint32_t root_dir_sectors = get_root_dir_sectors(hdr);
+
+    printf("FAT%d filesystem\n", get_fat_version(diskimg_path));
     printf("BytsPerSec = %u\n", hdr->BPB_BytsPerSec);
     printf("SecPerClus = %u\n", hdr->BPB_SecPerClus);
     printf("RsvdSecCnt = %u\n", hdr->BPB_RsvdSecCnt);
-    printf("FATsSecCnt = ?\n");
-    printf("RootSecCnt = ?\n");
-    printf("DataSecCnt = ?\n");
-
-    /*
-     * Print the contents of the first cluster.
-     */
-    hexdump(image, sizeof(*hdr));
+    printf("FATsSecCnt = %u\n", hdr->BPB_NumFATs * fat_size);
+    printf("RootSecCnt = %u\n", root_dir_sectors);
+    printf("DataSecCnt = %u\n", get_data_sectors(hdr, root_dir_sectors, fat_size));
 
     munmap(image, size);
 }
